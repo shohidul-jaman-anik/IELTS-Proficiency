3b06b64e073f344d57a61e19fa0f798f39493362
Md Anik Hossain <anikh499@gmail.com>
2022-07-22 21:32:40 +0600

    post and delete done

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-
 async function run() {
     try {
         await client.connect();
@@ -29,7 +28,35 @@ async function run() {
             res.send(result)
         })
 
-       
+        // Post data into database
+        app.post('/addData', async (req, res) => {
+            const query = req.body
+            const result = await ApiCollection.insertOne(query)
+            res.send(result)
+        })
+
+        // delete api
+        app.delete('/api/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await ApiCollection.deleteOne(query)
+            res.send(result)
+        })
+
+        // update api
+        app.put('/api/:id', async (req, res) => {
+            const id = req.params.id
+            console.log(id)
+            const data = req.body
+            console.log(data)
+            const query = { _id: ObjectId(id) }
+            const upsert = { upsert: true }
+            const updateDoc = {
+                $set: data
+            }
+            const result = await ApiCollection.updateOne(query, updateDoc, upsert)
+            res.send(result)
+        })
 
         // post user signIn Information
         app.post('/form', async (req, res) => {
